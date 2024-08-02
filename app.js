aac5feecc42ac2c6188d9ecbd1aa2c310b1dc0a2
Ricardo Sonnenberg <sonnenberg.caco@gmail.com>
2024-08-02 01:25:30 -0300

    Applying changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
         }
     })
@@ -15,11 +15,13 @@ let contents = document.querySelectorAll('section')
 
 let activePage = window.location.href;
 let navLinks = document.querySelectorAll('#nav-itens a')
-
+let navbar = document.querySelector('#nav-itens');
+let icon = document.querySelector('#ham-icon');
+let ham = document.querySelector('.ham');
 navLinks.forEach(links => {
     links.classList.remove('active');
 
-    if (links.href == activePage){
+    if (links.href == activePage) {
         links.classList.add('active');
     }
 })
@@ -31,16 +33,44 @@ window.onscroll = () => {
         let height = cont.offsetHeight;
         let id = cont.getAttribute('id');
 
-        if (top > offset && top < offset + height){
+        if (top > offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
 
-                let teste = document.querySelector('#nav-itens a[href*='+ id +']');
+                let teste = document.querySelector('#nav-itens a[href*=' + id + ']');
                 teste.classList.add('active')
             })
 
-            
+
         }
     })
+
 }
+
+window.addEventListener('click', function(event) {
+    if (event.target !== navbar && event.target !== ham) {
+        navbar.classList.remove('show-menu');
+        navbar.classList.add('hide-menu');
+        icon.className = 'fa-solid fa-bars';
+    }
+  })
+
+
+
+function toggleMenu() {
+    
+    
+    if (navbar.classList.value == 'hide-menu') {
+        navbar.classList.remove('hide-menu');
+        navbar.classList.add('show-menu');
+        icon.className = 'fa-solid fa-xmark';
+        
+    } else {
+        navbar.classList.remove('show-menu');
+        navbar.classList.add('hide-menu');
+        icon.className = 'fa-solid fa-bars';
+        
+    }
+}
+
 
